73594f04ecfc45d9057e3598d12db3fcc9c21402
nathanbrn <breno_nathanael@hotmail.com>
2022-09-05 23:04:40 -0300

    js

--- src/js/navPages.js
+++ src/js/navPages.js
@@ -1,19 +1,38 @@
-function salgados() {
-    window.location.href = "src/pages/salgados.html";
-}
-function sanduiches() {
-    window.location.href = "src/pages/sanduiches.html";
-}
-function petiscos() {
-    window.location.href = "src/pages/petiscos.html";
-}
-function bebidas() {
-    window.location.href = "src/pages/bebidas.html";
-}
-function outros() {
-    window.location.href = "src/pages/outros.html";
-}
-
 function voltarIndex() {
     window.location.href = "../../index.html";
 }
+
+
+document.querySelectorAll("button").forEach( function(button) {
+
+    
+    button.addEventListener("click", function(event) {
+    const pages = event.target || event.srcElement;
+    const id = pages.id;
+
+    if(id === "index")
+        window.location.href = "../../"+id+".html";
+    else 
+        window.location.href = "src/pages/"+id+".html";
+  });
+
+});
+
+
+
+// function salgados() {
+//     window.location.href = "src/pages/salgados.html";
+// }
+// function sanduiches() {
+//     window.location.href = "src/pages/sanduiches.html";
+// }
+// function petiscos() {
+//     window.location.href = "src/pages/petiscos.html";
+// }
+// function bebidas() {
+//     window.location.href = "src/pages/bebidas.html";
+// }
+// function outros() {
+//     window.location.href = "src/pages/outros.html";
+// }
+
