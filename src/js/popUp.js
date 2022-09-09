08e930f3cdbef7508eb675eb94cc42ece0bb39bd
nathanbrn <breno_nathanael@hotmail.com>
2022-09-09 02:18:41 -0300

    pop_up

---
+++ src/js/popUp.js
@@ -0,0 +1,13 @@
+function abrirPop() {
+
+    const pop = document.getElementById('pop-up');
+    pop.style.display = 'block';
+
+}
+
+function fechar() {
+
+    const pop = document.getElementById('pop-up');
+    pop.style.display = 'none';
+  
+}
