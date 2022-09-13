e4b61925685e2eeec495834e5b5af6e20332531b
nathanbrn <breno_nathanael@hotmail.com>
2022-09-12 22:54:19 -0300

    botaoCopiar

---
+++ src/js/scriptCopy.js
@@ -0,0 +1,17 @@
+const pix = document.querySelector('.chave_pix');
+const buttonCopy = document.getElementById('copy');
+
+buttonCopy.addEventListener('click', copiarTexto);
+
+function copiarTexto(e) {
+
+   navigator.clipboard.writeText(pix.innerHTML);
+   buttonCopy.classList.add('ativo');
+   buttonCopy.innerText = 'Copiado ✅';
+
+};
+
+setTimeout(() => {
+    buttonCopy.classList.remove('ativo');
+    buttonCopy.innerText = 'Copiar chave PIX ➣';
+}, 5000);
