3a96a06660f1ef7a045cc3a76c8f3bc1561fab73
Breno <breno_nathanael@hotmail.com>
2023-01-02 15:06:40 -0300

    site reformado

--- src/js/products.js
+++ src/js/products.js
@@ -1,27 +1,43 @@
-function updateProductsSanduiches(productsData) {
-    const menuLeft = document.getElementById("menu_left");
-    const menuRight = document.getElementById("menu_right");
+fetch("../../data/products.json")
+    .then(res => {
+        return res.json();
+    })
+    .then(jsonBody => {
+        return jsonBody.products;
+    })
+    .then(products => {
+        return products[0].salgados;
+    })
+    .then(salgados => {
+        return salgados[0].pasteis;
+    })
+    .then(listPastel => {
+        return listPastel.map((pastel) => {
+            return pastel;
+        })
+    })
+    .then(pastel => {
+        const menu = document.getElementById("menu");
+        const nomesPasteis = pastel.map(nomes => nomes.name);
+        const precosPasteis = pastel.map(nomes => nomes.price);
 
-    menuLeft.innerHTML = productsData.products.sanduiches.map(sanduiche => {
-        // return `
-        //     <section class="item">
-        //         <div class="titulo_descricao">
-        //             <h2 class="title">${sanduiche.name}</h2>
-        //             <p class="descricao">${sanduiche.ingredients}</p>
-        //         </div>
-        //         <div class="preco">
-        //             <p>${sanduiche.price}</p>
-        //         </div>
-        //     </section>
-        // `;
-
-        console.log(sanduiche);
-    }).join("");
-
-}
-
-
-(async () => {
-    const productsData = await fetchProductsData();
-    updateProductsSanduiches(productsData);
-})()
+        for(let i = 0; i < nomesPasteis.length; i++) {
+            console.log(nomesPasteis[i]+" - "+precosPasteis[i]);
+            menu.innerHTML = `
+                <div class="container_item">
+                    <section class="item">
+                        <div class="titulo_descricao">
+                            <h2 class="title">${nomesPasteis[i]}</h2>
+                            <p class="descricao">Lorem ipsum dolor sit amet consectetur, adipisicing elit. </p>
+                        </div>
+                        <div class="preco">
+                            <p>${precosPasteis[i]}</p>
+                        </div>
+                    </section>
+                </div>
+            `;
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+    });
--- src/js/requisitionProducts.js
+++
@@ -1,5 +0,0 @@
-async function fetchProductsData() {
-    const url = "https://raw.githubusercontent.com/nathanbrn/big/main/data/products.json";
-    const fetching = await fetch(url);
-    return await fetching.json();
-}
